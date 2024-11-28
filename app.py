b43d41823b5fbda46bff1306a82060639e8d6074
cassy <cassandra.flemming@telekom.de>
2024-11-28 15:22:47 +0100

    I added an new route :)

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   return "Hello World"
+
+
+@app.route('/sammy/')
+def hello_world2():
+  return "Hello World 2"
+
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port = int("80"), debug = True)
+  app.run(host="0.0.0.0", port=int("80"), debug=True)
